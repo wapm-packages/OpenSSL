d668112510a84f740805a692bf68826420e1887d
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2019-05-19 13:37:41 +0200

    Compile `openssl` CLI

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ emconfigure ./Configure linux-generic64 --prefix=${PREFIX}
 
 sed -i 's|^CROSS_COMPILE.*$|CROSS_COMPILE=|g' Makefile
 
-emmake make -j 12 build_generated libssl.a libcrypto.a
+emmake make -j 12 build_generated libssl.a libcrypto.a apps/openssl
 
 rm -rf ${PREFIX}/include
 mkdir -p ${PREFIX}/include
@@ -21,6 +21,18 @@ cp -R include/openssl ${PREFIX}/include
 
 emcc libcrypto.a -o ${PREFIX}/libcrypto.wasm
 emcc libssl.a -o ${PREFIX}/libssl.wasm
+emcc apps/app_rand.o apps/apps.o apps/asn1pars.o apps/ca.o apps/ciphers.o \
+  apps/cms.o apps/crl.o apps/crl2p7.o apps/dgst.o apps/dhparam.o apps/dsa.o \
+  apps/dsaparam.o apps/ec.o apps/ecparam.o apps/enc.o apps/engine.o \
+  apps/errstr.o apps/gendsa.o apps/genpkey.o apps/genrsa.o apps/nseq.o \
+  apps/ocsp.o apps/openssl.o apps/opt.o apps/passwd.o apps/pkcs12.o \
+  apps/pkcs7.o apps/pkcs8.o apps/pkey.o apps/pkeyparam.o apps/pkeyutl.o \
+  apps/prime.o apps/rand.o apps/rehash.o apps/req.o apps/rsa.o apps/rsautl.o \
+  apps/s_cb.o apps/s_client.o apps/s_server.o apps/s_socket.o apps/s_time.o \
+  apps/sess_id.o apps/smime.o apps/speed.o apps/spkac.o apps/srp.o apps/ts.o \
+  apps/verify.o apps/version.o apps/x509.o libssl.a libcrypto.a \
+  -o ${PREFIX}/openssl.wasm \
+  -s ERROR_ON_UNDEFINED_SYMBOLS=0
 
 cd ..
 rm -rf openssl-${OPENSSL_VERSION}*
