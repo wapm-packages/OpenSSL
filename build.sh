1cad482486c63aa81610553cdf6ef291d11f237a
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2019-06-13 09:14:53 +0200

    Clean up of build script

--- build.sh
+++ build.sh
@@ -5,35 +5,38 @@
 OPENSSL_VERSION=1.1.0h
 PREFIX=`pwd`
 
+echo "Download source code"
+rm -rf openssl-${OPENSSL_VERSION}*
 wget https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
 tar xf openssl-${OPENSSL_VERSION}.tar.gz
 cd openssl-${OPENSSL_VERSION}
 
+echo "Configure"
+make clean
 emconfigure ./Configure linux-x32 -no-asm -no-threads --prefix=${PREFIX} || exit $?
 
 sed -i 's|^CROSS_COMPILE.*$|CROSS_COMPILE=|g' Makefile
 
+echo "Build"
 emmake make -j 12 build_generated libssl.a libcrypto.a apps/openssl
 
 rm -rf ${PREFIX}/include
 mkdir -p ${PREFIX}/include
 cp -R include/openssl ${PREFIX}/include
 
+echo "Generate libraries .wasm files"
 emcc libcrypto.a -o ${PREFIX}/libcrypto.wasm
 emcc libssl.a -o ${PREFIX}/libssl.wasm
-emcc apps/app_rand.o apps/apps.o apps/asn1pars.o apps/ca.o apps/ciphers.o \
-  apps/cms.o apps/crl.o apps/crl2p7.o apps/dgst.o apps/dhparam.o apps/dsa.o \
-  apps/dsaparam.o apps/ec.o apps/ecparam.o apps/enc.o apps/engine.o \
-  apps/errstr.o apps/gendsa.o apps/genpkey.o apps/genrsa.o apps/nseq.o \
-  apps/ocsp.o apps/openssl.o apps/opt.o apps/passwd.o apps/pkcs12.o \
-  apps/pkcs7.o apps/pkcs8.o apps/pkey.o apps/pkeyparam.o apps/pkeyutl.o \
-  apps/prime.o apps/rand.o apps/rehash.o apps/req.o apps/rsa.o apps/rsautl.o \
-  apps/s_cb.o apps/s_client.o apps/s_server.o apps/s_socket.o apps/s_time.o \
-  apps/sess_id.o apps/smime.o apps/speed.o apps/spkac.o apps/srp.o apps/ts.o \
-  apps/verify.o apps/version.o apps/x509.o libssl.a libcrypto.a \
+
+echo "Link"
+emcc apps/*.o libssl.a libcrypto.a \
   -o ${PREFIX}/openssl.wasm \
   -s ALLOW_MEMORY_GROWTH=1 \
   -s ERROR_ON_UNDEFINED_SYMBOLS=0
+chmod +x ${PREFIX}/openssl.wasm || exit $?
 
+echo "Clean"
 cd ..
 rm -rf openssl-${OPENSSL_VERSION}*
+
+echo "Done"
