8456cdaf93b29b88a124674d2916625431bcfb45
Syrus Akbary <me@syrusakbary.com>
2021-03-25 09:54:34 +0100

    Added lib files for openssl

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # Based on code from https://github.com/TrueBitFoundation/wasm-ports/blob/master/openssl.sh
 
 OPENSSL_VERSION=1.1.0l
+# OPENSSL_VERSION=1.1.1i
 PREFIX=`pwd`
 
 DIRECTORY="openssl-${OPENSSL_VERSION}"
@@ -17,20 +18,25 @@ cd openssl-${OPENSSL_VERSION}
 
 echo "Configure"
 make clean
-wasiconfigure ./Configure linux-x32 -no-asm -static -no-sock -no-afalgeng -DOPENSSL_SYS_NETWARE -DSIG_DFL=0 -DSIG_IGN=0 -DHAVE_FORK=0 -DOPENSSL_NO_AFALGENG=1 -DOPENSSL_NO_SPEED=1 || exit $?
+wasiconfigure ./Configure linux-x32 -no-asm -no-threads -static -no-afalgeng -DOPENSSL_SYS_NETWARE -DSIG_DFL=0 -DSIG_IGN=0 -DHAVE_FORK=0 -DOPENSSL_NO_AFALGENG=1 -DOPENSSL_NO_SPEED=1 -DNO_SYSLOG || exit $?
+# 1.1.1i
+# wasiconfigure ./Configure linux-x32 -no-asm -no-dso -no-threads -no-ui-console -static -no-afalgeng -DOPENSSL_SYS_NETWARE -DSIG_DFL=0 -DSIG_IGN=0 -DHAVE_FORK=0 -DOPENSSL_NO_AFALGENG=1 -DOPENSSL_NO_SPEED=1 -DNO_SYSLOG || exit $?
 
 cp ../progs.h apps/progs.h
 
 sed -i 's|^CROSS_COMPILE.*$|CROSS_COMPILE=|g' Makefile
 
 echo "Build"
-wasimake make -j12 build_generated libssl.a libcrypto.a apps/openssl
+wasimake make -j12 build_generated libssl.a libcrypto.a # apps/openssl
 
 rm -rf ${PREFIX}/include
 mkdir -p ${PREFIX}/include
 cp -R include/openssl ${PREFIX}/include
 
-cp -R apps/openssl.wasm ../
+cp libssl.a ${PREFIX}/lib
+cp libcrypto.a ${PREFIX}/lib
+
+# cp -R apps/openssl.wasm ../
 
 # echo "Generate libraries .wasm files"
 # wasicc libcrypto.a -o ${PREFIX}/libcrypto.wasm
